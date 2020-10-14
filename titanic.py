ec8e9d128eb9d9ee8af8e9fe13beb91775eb7b9e
선동환 <sunehd564@gmail.com>
2020-10-14 18:03:25 +0900

    confusion_marix 실습

--- titanic.py
+++ titanic.py
@@ -2,17 +2,36 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
+from sklearn.base import BaseEstimator
+from sklearn.metrics import confusion_matrix
+
 pd.set_option('display.max_row', 500)
 pd.set_option('display.max_columns', 100)
+
+class MyDummyClassifier(BaseEstimator):
+    def fit(self, X, y=None):
+        pass
+    def predict(self, X):
+        pred = np.zeros((X.shape[0], 1))
+        for i in range(X.shape[0]):
+            if X['Sex'].iloc[i] == 1:
+                pred[i] = 0
+            else:
+                pred[i] = 1
+
+        return pred
+
 
 def get_category(age):
     cat = ''
@@ -104,6 +123,28 @@ def GridSearch(clf, x_data, y_data, x_test_data, y_test_data, parameters, cv):
     dpredictions = best_dclf.predict(x_test_data)
     accuracy = accuracy_score(y_test_data, dpredictions)
     print('테스트 셋에서의 정확도:{0:.4f}'.format(accuracy))
+def svm_GridSearch(x_data, y_data, x_test_data, y_test_data, cv):
+    best_score = 0
+    for gamma in [0.001, 0.01, 0.1, 1, 10, 100]:
+        for C in [0.001, 0.01, 0.1, 1, 10, 100]:
+            svm = SVC(gamma= gamma, C=C)
+
+            scores = cross_val_score(svm, x_data, y_data, cv=cv)
+            score = np.mean(scores)
+
+            if score > best_score:
+                best_score = score
+                best_parameter = {'gamma': gamma, 'C': C}
+
+    svm = SVC(**best_parameter)
+    svm.fit(x_data, y_data)
+    train_score = svm.score(x_data, y_data)
+    test_score = svm.score(x_test_data, y_test_data)
+    print('best_parameter:', best_parameter)
+    print('SVM train_Score: {0} \n test_Score: {1} '.format(train_score, test_score))
+
+dumy = MyDummyClassifier()
+
 
 parameters = {'max_depth':[2, 3, 5, 10], 'min_samples_split':[2, 3, 5], 'min_samples_leaf':[1, 5, 8]}
 titanic_df = pd.read_csv('./titanic/train.csv')
@@ -119,11 +160,16 @@ dt_cls = DecisionTreeClassifier(random_state=11)
 rf_cls = RandomForestClassifier(random_state=11)
 #lr_cls = LogisticRegression()
 
-
+dumy.fit(X_train, Y_train)
+dumy_pred = dumy.predict(X_test)
+print(confusion_matrix(Y_test, dumy_pred))
+print('{0}'.format(accuracy_score(Y_test, dumy_pred)))
 exec_Kfold(dt_cls, fold=5)
 exec_Kfold(rf_cls, fold=5)
-cross_var(dt_cls, X_titanic, Y_titanic, 5)
-cross_var(rf_cls, X_titanic, Y_titanic, 5)
+cross_var(dt_cls, X_train, Y_train, 5)
+cross_var(rf_cls,  X_train, Y_train, 5)
+
 #exec_Kfold(lr_cls, fold=5)
-GridSearch(dt_cls, X_titanic, Y_titanic, X_test, Y_test, parameters, 5)
-GridSearch(rf_cls, X_titanic, Y_titanic, X_test, Y_test, parameters, 5)
+GridSearch(dt_cls,  X_train, Y_train, X_test, Y_test, parameters, 5)
+GridSearch(rf_cls,  X_train, Y_train, X_test, Y_test, parameters, 5)
+svm_GridSearch( X_train, Y_train, X_test, Y_test, 5)
